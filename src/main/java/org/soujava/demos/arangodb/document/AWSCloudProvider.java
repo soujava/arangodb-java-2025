17859271506d93c155a301d0d3d9629d13983052
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-02 20:44:18 +0000

    chore: create awscloud provider
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/org/soujava/demos/arangodb/document/AWSCloudProvider.java
+++ src/main/java/org/soujava/demos/arangodb/document/AWSCloudProvider.java
@@ -4,12 +4,11 @@ import jakarta.nosql.Column;
 import jakarta.nosql.Entity;
 import net.datafaker.Faker;
 
+import java.util.UUID;
+
 @Entity
 public class AWSCloudProvider extends CloudProvider {
 
-
-    @Column
-    private String appName;
 
     @Column
     private String accountId;
@@ -18,7 +17,12 @@ public class AWSCloudProvider extends CloudProvider {
 
     public static AWSCloudProvider of(Faker faker) {
         var aws = faker.aws();
-
+        var cloudProvider = new AWSCloudProvider();
+        cloudProvider.region = aws.region();
+        cloudProvider.region = aws.region();
+        cloudProvider.id = UUID.randomUUID().toString();
+        cloudProvider.accountId = aws.accountId();
+        return cloudProvider;
 
     }
 
